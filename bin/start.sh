5e3eff90367cb87e30c22c169e0825480c6a9f08
brightvip <brightvip@msn.com>
2022-10-06 17:12:35 +0800

    1
    1

--- bin/start.sh
+++ bin/start.sh
@@ -106,11 +106,11 @@ cat << EOF >/usr/app/lib/nginx/websocket_proxy.conf.template
        return 301 https://\$host\$request_uri;
     }
     proxy_buffering on;
-    proxy_buffer_size 1k;
-    proxy_buffers 1 1k;
-    proxy_busy_buffers_size 1k;
+    proxy_buffer_size 4k;
+    proxy_buffers 4 4k;
+    proxy_busy_buffers_size 4k;
     
-    #client_body_buffer_size 1k;
+    client_body_buffer_size 8k;
     
     proxy_connect_timeout 75s;
     proxy_redirect off;
@@ -143,7 +143,7 @@ cat << EOF >/usr/app/lib/nginx/grpc_proxy.conf.template
         return 404;
     }
     
-    #client_body_buffer_size 1k;
+    client_body_buffer_size 8k;
     
     client_body_timeout 300s;
     client_max_body_size 0;
@@ -153,7 +153,7 @@ cat << EOF >/usr/app/lib/nginx/grpc_proxy.conf.template
     grpc_read_timeout 300s;
     grpc_send_timeout 300s;
     grpc_socket_keepalive on;
-    grpc_buffer_size 1k;
+    grpc_buffer_size 4k;
     grpc_pass proxyPass;
   }
 EOF

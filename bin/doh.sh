818a5933aad5babfd249040fab46b6e040fe839d
brightvip <brightvip@msn.com>
2022-10-11 18:55:20 +0800

    1
    1

--- bin/doh.sh
+++ bin/doh.sh
@@ -24,8 +24,5 @@ EOF
  sed -e 's:path:'"${WSPATH}/dns-query"':' -e 's/proxyPass/https:\/\/dns.google\/dns-query/' /usr/app/lib/nginx/doh.hp.conf.template > /usr/app/lib/nginx/doh.hp.g
  sed -i '35 r /usr/app/lib/nginx/doh.hp.g' /etc/nginx/conf.d/default.conf
  
- 
- sleep 10s
- nginx  -s reload
 }
 conf

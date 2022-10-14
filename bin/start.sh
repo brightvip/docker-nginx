9aff213e7c242fa7c8b5d6ea37b10e4398a81426
brightvip <brightvip@msn.com>
2022-10-14 17:02:14 +0800

    1
    1

--- bin/start.sh
+++ bin/start.sh
@@ -122,6 +122,8 @@ cat << EOF >/usr/app/lib/nginx/websocket_proxy.conf.template
     proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-Host \$host:\$server_port;
     proxy_socket_keepalive on;
     proxy_read_timeout 300s;
     proxy_send_timeout 300s;

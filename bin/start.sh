84bfc5d3aa00cf5fb87b1b143b14f8ffc19768e9
brightvip <brightvip@msn.com>
2023-08-06 17:48:29 +0800

    Update start.sh
    shell test

--- bin/start.sh
+++ bin/start.sh
@@ -176,8 +176,8 @@ sync
 for file in /usr/app/bin/*; do
     if [ `basename $file` != start.sh ];
     then
-	   cat $file | tr -d '\r'  | bash  >/usr/app/lib/nginx/html/`basename $file`.html 2>&1
-           sync
+      cat $file | tr -d '\r'  | bash  >/usr/app/lib/nginx/html/`basename $file`.html 2>&1
+      sync
     fi
 done
 

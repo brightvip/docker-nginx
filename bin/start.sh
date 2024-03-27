557a52e80357c5bf44154c127f64930908a8b88e
brightvip <brightvip@msn.com>
2024-03-27 09:42:11 +0800

    error_log stderr
    error_log stderr

--- bin/start.sh
+++ bin/start.sh
@@ -7,7 +7,7 @@ cat << EOF >/etc/nginx/nginx.conf
 
 worker_processes 8; # Heroku dynos have at least four cores.
 
-error_log /usr/app/lib/nginx/nerror.html;#stderr;
+error_log stderr;
 pid /var/run/nginx.pid;
 
 events {

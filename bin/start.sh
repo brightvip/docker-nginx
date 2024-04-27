2d5abcfb694c621261295ec5d50323a6139f536e
brightvip <brightvip@msn.com>
2024-04-27 09:13:47 +0800

    Update start.sh
    SSL

--- bin/start.sh
+++ bin/start.sh
@@ -47,7 +47,9 @@ if [ -z "$PORT" ]; then
     export PORT=8080
 fi
 echo "PORT=: $PORT"
-cat << EOF >/etc/nginx/conf.d/default.conf
+
+if [ -z "$NGINX_SSL" ];then
+ cat << EOF >/etc/nginx/conf.d/default.conf
 server {
   listen $PORT;
   listen [::]:$PORT;
@@ -88,7 +90,55 @@ server {
 
 }
 EOF
+else
+ cat << EOF >/usr/app/ssl/server.crt
+$SSLCERTIFICATE
+EOF
+ cat << EOF >/usr/app/ssl/server.key
+$SSLCERTIFICATEKEY
+EOF
+ cat << EOF >/etc/nginx/conf.d/default.conf
+server {
+  listen $PORT ssl;
+  listen [::]:$PORT ssl;
+  server_name $SERVERNAME;
+  ssl    on;
+  ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+  ssl_ciphers ECDHE-RSA-AES256-SHA384:AES256-SHA256:RC4:HIGH:!MD5:!aNULL:!eNULL:!NULL:!DH:!EDH:!AESGCM;
+  ssl_prefer_server_ciphers on;
+  ssl_session_cache shared:SSL:40m;
+  ssl_session_timeout 60m;
+  ssl_certificate /usr/app/ssl/server.crt;
+  ssl_certificate_key /usr/app/ssl/server.key;
+  location / {
+    if (\$http_x_forwarded_proto != "https") {
+      return 301 https://\$host\$request_uri;
+    }
+    proxy_connect_timeout 120;
+    proxy_read_timeout 86400;
+    proxy_pass https://github.com;
+    proxy_set_header Host github.com;
+  }
+  location /url {
+      return 301 https://\$host/html/5m;
+  }
+  location /html/ {
+    if (\$http_x_forwarded_proto != "https") {
+      return 301 https://\$host\$request_uri;
+    }
+    root  /usr/app/lib/nginx;
+    index  index.html index.htm;
+    
+  }
 
+
+
+
+
+
+}
+EOF
+fi
 
 #/etc/nginx/conf.d/upstream.conf
 touch /etc/nginx/conf.d/upstream.conf
